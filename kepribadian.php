56c4bba1d20c62b1ca4f639c0b7fa274e4fdf09d
yayangIzza <149374422+yayangIzza@users.noreply.github.com>
2023-10-30 19:59:11 +0700

    Add files via upload

---
+++ kepribadian.php
@@ -0,0 +1,30 @@
+<?php
+
+echo "Nama  : Yayang Izzatul Haq<br>";
+echo "NIM   : 22205052<br>";
+echo "Kelas : PBO<br>";
+
+
+echo "=====cek kepribadian======<br>";
+
+echo "POLIH WARNA FAVORITEMU : BIRU<br>";
+echo "NAMA KAMU : RIZKI<br>";
+
+echo "<background color='red'>merah</font><br>";
+echo "<background color='green'>hijau</font><br>";
+echo "<background color='yellow'>kuning</font><br>";
+echo "<backgroud color='blue'>biru</font><br>";
+echo "<backgroud color='purple'>ungu</font><br>";
+
+echo "=====HASIL TES KEPRIBADIAN RIZKI=====<br>";
+echo "warna favorite adalah BIRU<br>";
+echo "1. menyenangkan,<br>";
+echo "2. bijaksana,<br>";
+echo "3. pembawaan diri tenang saat menghadapi masalah,<br>";
+echo "4. dinamis,<br>";
+echo "5. senang berbagi,<br>";
+echo "6. bersahabat,<br>";
+echo "7. tidak terlalu suka menjadi sorotan banyak orang,<br>";
+echo "2. menyembunyikan perasaan karna karakternya yang cenderung mencari jalan damai,<br>";
+
+?>
